5b8bf1be92febe37701f6cb7915dd6dbda376bd4
Clemens-Alexander Brust <ikosa.de@gmail.com>
2020-02-16 00:05:59 +0100

    Added support for tox, added basic setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="mlre",
+    version="0.0.1",
+    author="Clemens-Alexander Brust",
+    author_email="ikosa.de@gmail.com",
+    description="Machine Learning Research Environment",
+    url="https://github.com/cabrust/mlre",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=["requests==2.22.0"]
+)
