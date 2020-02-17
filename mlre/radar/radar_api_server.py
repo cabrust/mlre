216cd91f46f911d623e7738585d49714d614a409
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2020-02-17 16:41:28 +0100

    radar_api_server: Started work

--- mlre/radar/radar_api_server.py
+++ mlre/radar/radar_api_server.py
@@ -1,19 +1,32 @@
 """Server component for the radar API."""
 import typing
 
-from flask import Flask
+from flask import Flask, jsonify
 
+import mlre
 from mlre.radar import radar_database
 
 
-def create_api_server(database: radar_database.RadarDatabase,   # type: ignore
-                      configuration: typing.Optional[typing.Mapping[str, typing.Any]] = None) -> Flask:
+def create_api_server(database: radar_database.RadarDatabase,   # type: ignore  # pylint: disable=W0613
+                      configuration: typing.Optional[typing.Mapping[str, typing.Any]] = None)\
+        -> Flask:
+    """Creates an instance of the API server.
+
+    Args:
+        database: An instance of the radar event and client info database.
+        configuration: Optional flask configuration values.
+    """
     api_server = Flask(
         __name__, instance_relative_config=True)  # pragma: no mutate
 
     # Apply configuration if possible
     if configuration is not None:  # type: ignore
         api_server.config.from_mapping(configuration)  # type: ignore
+
+    @api_server.route('/version')
+    def get_api_version() -> str:  # type: ignore  # pylint: disable=W0612
+        """Give api server and mlre versions."""
+        return jsonify({'api': '1', 'mlre': mlre.__version__})  # type: ignore
 
     return api_server
 
--- tests/test_radar_api_server.py
+++ tests/test_radar_api_server.py
@@ -2,24 +2,38 @@
 import typing
 import unittest
 
+import mlre
 from mlre.radar import radar_api_server, radar_database
 
-_TEST_CONFIGURATION:  typing.Dict[str, str] = {
+_TEST_CONFIGURATION: typing.Dict[str, str] = {
     "TEST_CFG_1": "test_cfg_1_val", "TEST_CFG_2": "test_cfg_2_val"}
 
 
 class TestRadarAPIServer(unittest.TestCase):
+    """Test for radar API server component."""
+
     def setUp(self) -> None:
         """Creates an instance of the API server to work on."""
         self.database = radar_database.RadarDatabase()
         self.api_server = radar_api_server.create_api_server(
             self.database, _TEST_CONFIGURATION)
 
-    """Test for radar API server component."""
+        self.api_test_client = self.api_server.test_client().__enter__()  # type: ignore
+
+    def tearDown(self) -> None:
+        """Tears down the flask test client."""
+        self.api_test_client.__exit__(None, None, None)
 
     def test_configuration(self) -> None:
-        """This test checks if the supplied configuration was used."""
+        """Check if the supplied configuration was used."""
         for key, value in _TEST_CONFIGURATION.items():
             actual_value: str = typing.cast(
                 str, self.api_server.config.get(key))
             self.assertEqual(value, actual_value)
+
+    def test_get_api_version(self) -> None:
+        """Test if the reported API and MLRE versions are correct."""
+        result = self.api_test_client.get('/version').get_json()
+        self.assertEqual('1', result['api'], "There is only this API version.")
+        self.assertEqual(mlre.__version__,
+                         result['mlre'], "MLRE version should match module.")
