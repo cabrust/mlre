7871b0d4403d0e87b825cb37eac5941c9ac3e496
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2020-02-17 16:39:32 +0100

    Changed versioning location

---
+++ mlre/version.py
@@ -0,0 +1,4 @@
+"""This file determines the MLRE version"""
+__version__ = "0.0.2"
+
+__all__ = ["__version__"]
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+from distutils import util
+
 import setuptools
+
+main_ns = {}
+ver_path = util.convert_path('mlre/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
 
 setuptools.setup(
     name="mlre",
-    version="0.0.1",
+    version=main_ns['__version__'],
     author="Clemens-Alexander Brust",
     author_email="ikosa.de@gmail.com",
     description="Machine Learning Research Environment",
