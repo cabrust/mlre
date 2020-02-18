eeca46b5f3f0101fd1b68dcce13ba55e65e7efbc
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2020-02-18 14:34:31 +0100

    radar_api_server: Added report_event API

--- mlre/radar/radar_api_server.py
+++ mlre/radar/radar_api_server.py
@@ -1,13 +1,14 @@
 """Server component for the radar API."""
 import typing
+import uuid
 
-from flask import Flask, jsonify
+from flask import Flask, request
 
 import mlre
-from mlre.radar import radar_database
+from mlre.radar import radar_common, radar_database
 
 
-def create_api_server(database: radar_database.RadarDatabase,   # type: ignore  # pylint: disable=W0613
+def create_api_server(database: radar_database.RadarDatabase,   # type: ignore
                       configuration: typing.Optional[typing.Mapping[str, typing.Any]] = None)\
         -> Flask:
     """Creates an instance of the API server.
@@ -23,10 +24,27 @@ def create_api_server(database: radar_database.RadarDatabase,   # type: ignore  
     if configuration is not None:  # type: ignore
         api_server.config.from_mapping(configuration)  # type: ignore
 
-    @api_server.route('/version')
-    def get_version() -> str:  # type: ignore  # pylint: disable=W0612
+    @api_server.route('/version')  # type: ignore
+    def get_version() -> typing.Dict[str, str]:  # pylint: disable=W0612
         """Give api server and mlre versions."""
-        return jsonify({'api': '1', 'mlre': mlre.__version__})  # type: ignore
+        return {'api': '1', 'mlre': mlre.__version__}
+
+    @api_server.route('/report_event', methods=['POST'])  # type: ignore
+    def report_event() -> str:  # pylint: disable=W0612
+        # Decode request
+        request_json = request.json  # type: ignore
+        session_id: uuid.UUID = uuid.UUID(
+            request_json['session_id'])  # type: ignore
+        event_identifier: radar_common.EventIdentifier =\
+            radar_common.EventIdentifier(
+                *request_json['event_identifier'])  # type: ignore
+
+        freeze_frame: radar_common.FreezeFrameData =\
+            request_json['freeze_frame']  # type: ignore
+
+        # Make database call
+        database.insert_event(session_id, event_identifier, freeze_frame)
+        return ''
 
     return api_server
 
--- tests/test_radar_api_server.py
+++ tests/test_radar_api_server.py
@@ -1,9 +1,11 @@
 """Test for radar API server component."""
 import typing
 import unittest
+from unittest import mock
 
 import mlre
-from mlre.radar import radar_api_server, radar_database
+import test_radar_common
+from mlre.radar import radar_api_server
 
 _TEST_CONFIGURATION: typing.Dict[str, str] = {
     "TEST_CFG_1": "test_cfg_1_val", "TEST_CFG_2": "test_cfg_2_val"}
@@ -13,16 +15,23 @@ class TestRadarAPIServer(unittest.TestCase):
     """Test for radar API server component."""
 
     def setUp(self) -> None:
-        """Creates an instance of the API server to work on."""
-        self.database = radar_database.RadarDatabase()
+        """Creates an instance of the API server to work on and a mock database."""
+
+        # Create mocked database
+        self.patcher = mock.patch('mlre.radar.radar_database.RadarDatabase')
+        patched_database_type = self.patcher.start()
+        self.database = patched_database_type()
+
+        # Start test client
         self.api_server = radar_api_server.create_api_server(
             self.database, _TEST_CONFIGURATION)
 
         self.api_test_client = self.api_server.test_client().__enter__()  # type: ignore
 
     def tearDown(self) -> None:
-        """Tears down the flask test client."""
+        """Tears down the flask test client and database mock."""
         self.api_test_client.__exit__(None, None, None)
+        self.patcher.stop()
 
     def test_configuration(self) -> None:
         """Check if the supplied configuration was used."""
@@ -33,7 +42,34 @@ class TestRadarAPIServer(unittest.TestCase):
 
     def test_get_api_version(self) -> None:
         """Test if the reported API and MLRE versions are correct."""
-        result = self.api_test_client.get('/version').get_json()
+        response = self.api_test_client.get('/version')
+
+        self.assertEqual(200, response.status_code)
+
+        result = response.get_json()
         self.assertEqual('1', result['api'], "There is only this API version.")
         self.assertEqual(mlre.__version__,
                          result['mlre'], "MLRE version should match module.")
+
+    def test_report_event(self) -> None:
+        """Test if the event reporting API calls the database correctly."""
+        request_body = {"session_id": str(test_radar_common.TEST_SESSION_UUID),
+                        "event_identifier": test_radar_common.TEST_EVENT_IDENTIFIER,
+                        "freeze_frame": test_radar_common.TEST_EVENT_FREEZE_FRAME}
+
+        response = self.api_test_client.post(
+            '/report_event', json=request_body)
+
+        self.assertEqual(200, response.status_code)
+
+        self.assertEqual(1, len(self.database.method_calls),
+                         "Number of database calls should be 1")
+
+        # Test if method was called correctly
+        target_method, arguments, _ = self.database.method_calls[0]
+
+        self.assertEqual('insert_event', target_method)
+        self.assertEqual(test_radar_common.TEST_SESSION_UUID, arguments[0])
+        self.assertEqual(test_radar_common.TEST_EVENT_IDENTIFIER, arguments[1])
+        self.assertEqual(
+            test_radar_common.TEST_EVENT_FREEZE_FRAME, arguments[2])
