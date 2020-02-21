68be583d310408dc78c06c3de177b65bfbc8f2ef
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2020-02-21 12:49:37 +0100

    radar_api_client: Renamed session_uuid to session_id

--- mlre/radar/radar_api_client.py
+++ mlre/radar/radar_api_client.py
@@ -13,15 +13,15 @@ class APIClient:
 
     def __init__(self,
                  endpoint_url: str,
-                 session_uuid: uuid.UUID):
+                 session_id: uuid.UUID):
         """Connects to a radar server.
 
         Args:
             endpoint_url: URL to send requests to.
-            session_uuid: UUID (self-generated) of the current session.
+            session_id: UUID (self-generated) of the current session.
         """
         self._endpoint_url: str = endpoint_url
-        self._session_uuid: uuid.UUID = session_uuid
+        self._session_id: uuid.UUID = session_id
         self._has_reported_client_info: bool = False
 
     def get_api_version(self) -> typing.Optional[str]:
@@ -56,7 +56,7 @@ class APIClient:
 
         request_url = urllib.parse.urljoin(
             self._endpoint_url, "report_client_info")
-        request_body = {"session_id": str(self._session_uuid),
+        request_body = {"session_id": str(self._session_id),
                         "client_info": client_info}
 
         requests.post(request_url, json=request_body)
@@ -82,7 +82,7 @@ class APIClient:
                 "Make sure to report the client information before reporting any events.")
 
         request_url = urllib.parse.urljoin(self._endpoint_url, "report_event")
-        request_body = {"session_id": str(self._session_uuid),
+        request_body = {"session_id": str(self._session_id),
                         "event_identifier": event_identifier,
                         "freeze_frame": freeze_frame}
 
